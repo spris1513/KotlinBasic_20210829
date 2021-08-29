7f6e4b0a27463ff888672f5989f50606cef049e6
이진호 <spris1513@gmail.com>
2021-08-29 10:45:20 +0900

    토스트버튼 추가

--- app/src/main/java/com/neppplus/kotlinbasic_20210829/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20210829/MainActivity.kt
@@ -3,12 +3,19 @@ package com.neppplus.kotlinbasic_20210829
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toastBtn.setOnClickListener {
+//            토스트 - "반갑습니다." 문장 띄우기
+
+            Toast.makeText(this, "반갑습니다.", Toast.LENGTH_SHORT).show()
+        }
 
         LogBtn.setOnClickListener {
 
