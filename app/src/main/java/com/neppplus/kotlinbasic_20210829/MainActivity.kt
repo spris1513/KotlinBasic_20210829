06b37a0eafb8c95c805b77272aa84403102db2a1
이진호 <spris1513@gmail.com>
2021-08-29 10:22:42 +0900

    로그찍기 이벤트 예시

--- app/src/main/java/com/neppplus/kotlinbasic_20210829/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20210829/MainActivity.kt
@@ -2,10 +2,19 @@ package com.neppplus.kotlinbasic_20210829
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LogBtn.setOnClickListener {
+
+            Log.d("메인화면", "로그 버튼 눌림")
+
+        }
+
     }
 }
